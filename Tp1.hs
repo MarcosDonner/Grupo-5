1c86c0193ae3d7179b3a927dcb7750815e31081d
Unknown <alexmclm@gmail.com>
2018-05-06 18:23:04 -0300

    Agregue funcion para Test 22

--- Tp1.hs
+++ Tp1.hs
@@ -136,6 +136,8 @@ pruebaEventosTP2 = hspec $ do
     it " impactar transacción5 y luego transacción2 a pepe. Obteniendo saldo 8 en su billetera" $ impactar (transacción2.transacción5) pepe `shouldBe` nuevaBilletera 8 pepe
     describe " Usuario luego de Aplicar bloques de Transacciónes" $ do
     it " impactar el bloque1 en pepe , y su saldo debe ser de 18 monedas " $ comoQuedaSaldo bloque1 pepe  `shouldBe` nuevaBilletera 18 pepe
+    it " determinar saldo menor a N creditos, para pepe y lucho , mostrando el que quede con saldo mayor a 10: pepe"
+    $ usuarioConSaldoSegunNCreditos 10 bloque1 [pepe,lucho] `shouldBe` [pepe]
 
 nuevaBilletera :: Float -> Usuario -> Usuario
 nuevaBilletera otraBilletera unUsuario = unUsuario{billetera = otraBilletera}
@@ -150,3 +152,8 @@ bloque1 = [transacción1,transacción2,transacción2,transacción2,transacción3
 
 comoQuedaSaldo::  Bloque -> Usuario-> Usuario
 comoQuedaSaldo unBloque unUsuario  = foldr impactar unUsuario unBloque
+
+usuarioConSaldoSegunNCreditos :: Float -> Bloque -> [Usuario] ->  [Usuario]
+usuarioConSaldoSegunNCreditos unCredito unBloque unosUsuarios = filter ((>= unCredito).billetera.(comoQuedaSaldo unBloque)) unosUsuarios
+
+bloque2 = [transacción2,transacción2,transacción2,transacción2,transacción2]
